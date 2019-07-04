010a5eb338b6d41327d874c6c7ca96ff27dc3330
Gaurav Arora <gauravarora.daiict@gmail.com>
2019-07-04 20:27:50 +1000

    PUt weight correlation together, less neg value for corr,

--- models/save_weights.py
+++ models/save_weights.py
@@ -43,7 +43,11 @@ class SaveWeights:
       
   def set_stat(self, task, evalua, lay, gram, metric, metric_value, trainer, timeset, val):
     print("Adding training scalar: ", metric)
-    trainer._tensorboard.add_train_scalar("weight_stats/"+metric+"/"+str(task)+'/'+str(lay)+'/'+str(gram),
+    puttask = task
+    if metric == 'weight_corr':
+        puttask=''
+
+    trainer._tensorboard.add_train_scalar("weight_stats/"+metric+"/"+str(puttask)+'/'+str(lay)+'/'+str(gram),
             metric_value,
             timestep=timeset)
     val[metric] = metric_value
@@ -74,7 +78,7 @@ class SaveWeights:
 
               val={}
               dead,average_z,tot=self.get_zero_weights(current_activation)
-              timeset=tasks.index(evalua)
+              timeset=(tasks.index(evalua) + 1)
               val = self.set_stat(task, evalua, lay, gram, 'avg_zeros', average_z, trainer, timeset, val)
               val = self.set_stat(task, evalua, lay, gram, 'dead', average_z/tot, trainer, timeset, val)
               val = self.set_stat(task, evalua, lay, gram, 'dead_per', dead/tot, trainer, timeset, val)
@@ -108,7 +112,7 @@ class SaveWeights:
               val = self.set_stat(task, evalua, lay, gram, 'dead', average_z/tot, trainer, timeset, val)
               val = self.set_stat(task, evalua, lay, gram, 'dead_per', dead/tot, trainer, timeset, val)
               val = self.set_stat(task, evalua, lay, gram, 'total', tot, trainer, timeset, val)
-              val = self.set_stat(task, evalua, lay, gram, 'corr', -12345, trainer, timeset, val)
+              val = self.set_stat(task, evalua, lay, gram, 'corr', -1, trainer, timeset, val)
               val = self.set_stat(task, evalua, lay, gram, 'weight_corr', float(weight_corr), trainer, timeset, val)
               val['evaluate']=str(evalua)
               val['gram']=int(gram)
