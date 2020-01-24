27c6c504e14b87477be87f46551923c2af742a4e
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-01-24 19:00:35 +1100

    Add execution path run_all.sh

--- tensorboard-aggregator/run_agg.sh
+++ tensorboard-aggregator/run_agg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 path=$1
 exper=$2
-find $path -name '*_exper__*' | while read p; do python aggregator.py --path $p --operations mean  --store_path $path/evaluate_csv_agg --output csv --store_df --allowed_keys standard_evaluate --allowed_keys evaluate --allowed_keys restore_checkpoint --allowed_keys forgetting_metric; done > ${exper}_agg.log
+exec_python="/home/gaurava/miniconda3/envs/catastrophic/bin/python"
+find $path -name '*_exper__*' | while read p; do $exec_python aggregator.py --path $p --operations mean  --store_path $path/evaluate_csv_agg --output csv --store_df --allowed_keys standard_evaluate --allowed_keys evaluate --allowed_keys restore_checkpoint --allowed_keys forgetting_metric; done > ${exper}_agg.log
 #find $path -name '*_exper__*' | while read p; do python aggregator.py --path $p --operations mean  --store_path evaluate_csv_agg --output csv --store_df --allowed_keys standard_evaluate --allowed_keys evaluate --allowed_keys restore_checkpoint; done > ${exper}_agg.log
 #python process_results.py --path $path/evaluate_csv_agg/aggregates/  --exper $exper  --get_df > ${exper}_df.log
 #python process_results.py --path $path/evaluate_csv_agg/aggregates/  --exper $exper > ${exper}_new.log
