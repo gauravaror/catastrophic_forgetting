9913427d06a5ca20865fdc97d81fb27e588365af
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-02-02 11:00:21 +1100

    Take multiple quadratic penalities for EWC

--- models/ewc.py
+++ models/ewc.py
@@ -20,9 +20,6 @@ class EWC(object):
         self.params = {n: p for n, p in self.model.named_parameters() if p.requires_grad}
         self._means = {}
         self._precision_matrices = {}
-        for n, p in deepcopy(self.params).items():
-            p.data.zero_()
-            self._precision_matrices[n] = variable(p.data)
         self._len_dataset = 1
         self._old_len_dataset = 1
 
@@ -35,16 +32,21 @@ class EWC(object):
         self._old_len_dataset = self._len_dataset
         self.params = {n: p for n, p in self.model.named_parameters() if p.requires_grad}
         if datasets:
-            self._len_dataset += len(datasets)
+            self._len_dataset = len(datasets)
             self.dataset = datasets
             self._diag_fisher()
 
+        self._means[t] = {}
         for n, p in deepcopy(self.params).items():
-            self._means[n] = variable(p.data)
+            self._means[self.t][n] = variable(p.data)
 
     def _diag_fisher(self):
 
         self.model.eval()
+        self._precision_matrices[self.t] = {}
+        for n, p in deepcopy(self.params).items():
+            p.data.zero_()
+            self._precision_matrices[self.t][n] = variable(p.data)
 
         iterator = BucketIterator(batch_size=512, sorting_keys=[("tokens", "num_tokens")])
 
@@ -61,17 +63,20 @@ class EWC(object):
             loss.backward()
 
             for n, p in self.model.named_parameters():
-                if not p.grad is None and n in self._precision_matrices:
-                    #print(" grad ", self._precision_matrices[n], self._len_dataset, self._old_len_dataset)
-                    self._precision_matrices[n].data += p.grad.data ** 2
-                    self._precision_matrices[n].data = self._precision_matrices[n].data*(self._old_len_dataset/self._len_dataset)
+                #print("Moving params ", p.grad, n)
+                if (not (p.grad is None)) and n in self._precision_matrices[self.t]:
+                    #print(" grad ", self._precision_matrices[self.t][n], self._len_dataset, self._old_len_dataset)
+                    self._precision_matrices[self.t][n].data += ((p.grad.data ** 2)/self._len_dataset)
 
     def penalty(self, t:int):
         loss = 0
         if t == 1:
             return loss
-        for n, p in self.model.named_parameters():
-            if n in self._precision_matrices:
-              _loss = self._precision_matrices[n] * (p - self._means[n]) ** 2
-              loss += _loss.sum()
+        for key in self._precision_matrices.keys():
+            for n, p in self.model.named_parameters():
+                if n in self._precision_matrices[key]:
+                  _loss = self._precision_matrices[key][n] * (p - self._means[key][n]) ** 2
+                  #print("Loss", _loss, " precision ", self._precision_matrices[key][n], "  Diff  ", (p-self._means[key][n]))
+                  loss += _loss.sum()
+        print(loss)
         return loss
