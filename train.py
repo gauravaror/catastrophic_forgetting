fd86557e36f168335c85b8551fef91f66e56fdb5
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-03-02 18:52:24 +1100

    Remove join and diff class things

--- train.py
+++ train.py
@@ -23,10 +23,6 @@ import models.utils as utils
 import models.evaluate as eva
 #from torch.utils.tensorboard import SummaryWriter
 
-majority = {'subjectivity': 0.5, 'sst': 0.2534059946, 'trec': 0.188, 'cola': 0, 'ag': 0.25, 'sst_2c': 0.51}
-
-sota = {'subjectivity': 0.955, 'sst': 0.547, 'trec': 0.9807, 'cola': 0.341, 'ag' : 0.955 , 'sst_2c': 0.968}
-
 args = get_args()
 
 #writer=SummaryWriter(run_name)
@@ -64,15 +60,14 @@ joint_dev = []
 task_code=""
 labels_mapping={}
 for i in tasks:
-  task_code+=str("_"+str(i))
-  joint_train += train_data[i]
-  joint_dev += dev_data[i]
-  if args.diff_class:
+    task_code+=str("_"+str(i))
+    joint_train += train_data[i]
+    joint_dev += dev_data[i]
     vocabulary[i] = Vocabulary.from_instances(train_data[i] + dev_data[i])
     label_size = vocabulary[i].get_vocab_size('labels')
     label_indexes = {}
     for si in range(label_size):
-      label_indexes[si] = vocabulary[i].get_token_from_index(si, 'labels')
+        label_indexes[si] = vocabulary[i].get_token_from_index(si, 'labels')
     labels_mapping[i] = label_indexes
 print("Labels mapping :  ", labels_mapping)
 
@@ -127,71 +122,50 @@ if torch.cuda.is_available():
 
 overall_metrics = {}
 ostandard_metrics = {}
-if args.joint:
-  print("\nTraining task : Joint ", tasks)
-  trainer = Trainer(model=model,
-                  optimizer=optimizer,
-                  iterator=iterator,
-                  train_dataset=joint_train,
-                  validation_dataset=joint_dev,
-                  patience=args.patience,
-                  num_epochs=args.epochs,
-		  cuda_device=devicea)
-  trainer.train()
-  for i in evaluate_tasks:
-    print("\nEvaluating ", i)
-    sys.stdout.flush()
-    evaluate(model=model,
-	 instances=dev_data[i],
-	 data_iterator=iterator,
-	 cuda_device=devicea,
-	 batch_weight_key=None)
-else:
-  trainer = Trainer(model=model,
-                  optimizer=optimizer,
-                  iterator=iterator,
-                  train_dataset=train_data[i],
-                  validation_dataset=dev_data[i],
-                  validation_metric="-loss",
-                  num_serialized_models_to_keep=1,
-                  model_save_interval=1,
-                  serialization_dir=run_name,
-                  histogram_interval=100,
-                  patience=args.patience,
-                  num_epochs=args.epochs,
-                  cuda_device=devicea)
-  old_task_data = None
-  for tid,i in enumerate(train,1):
+trainer = Trainer(model=model,
+              optimizer=optimizer,
+              iterator=iterator,
+              train_dataset=train_data[i],
+              validation_dataset=dev_data[i],
+              validation_metric="-loss",
+              num_serialized_models_to_keep=1,
+              model_save_interval=1,
+              serialization_dir=run_name,
+              histogram_interval=100,
+              patience=args.patience,
+              num_epochs=args.epochs,
+              cuda_device=devicea)
+old_task_data = None
+for tid,i in enumerate(train,1):
     print("\nTraining task ", i)
     sys.stdout.flush()
-    if args.diff_class:
-      if args.pad_memory:
+    if args.pad_memory:
           model.encoder.add_target_pad()
-      training_ = True if i != 1 else False
-      normaliser = len(train_data[i])/args.bs
-      if args.ewc_normalise == 'length':
+    training_ = True if i != 1 else False
+    normaliser = len(train_data[i])/args.bs
+    if args.ewc_normalise == 'length':
           normaliser = len(train_data[i])
-      elif args.ewc_normalise == 'batches':
+    elif args.ewc_normalise == 'batches':
           normaliser = len(train_data[i])/args.bs
-      else:
+    else:
           normaliser  = 1
-      model.set_task(i, training=training_, normaliser=normaliser)
-      trainer._num_epochs = args.epochs
-      iterator.index_with(vocabulary[i])
-      trainer.train_data = train_data[i]
-      trainer._validation_data = dev_data[i]
-      trainer.model = model
-      trainer.iterator = iterator
-      trainer._validation_iterator = iterator
-      if i == 'cola':
+    model.set_task(i, training=training_, normaliser=normaliser)
+    trainer._num_epochs = args.epochs
+    iterator.index_with(vocabulary[i])
+    trainer.train_data = train_data[i]
+    trainer._validation_data = dev_data[i]
+    trainer.model = model
+    trainer.iterator = iterator
+    trainer._validation_iterator = iterator
+    if i == 'cola':
           trainer._validation_metric = 'average'
           trainer._metric_tracker._should_decrease = False
           trainer.validation_metric = '+average'
-      else:
+    else:
           trainer._validation_metric = 'loss'
           trainer._metric_tracker._should_decrease = True
           trainer.validation_metric = '-loss'
-      trainer._metric_tracker.clear()
+    trainer._metric_tracker.clear()
     if not args.majority:
       metrics = trainer.train()
       trainer._tensorboard.add_train_scalar("restore_checkpoint/"+str(i),
@@ -208,19 +182,19 @@ else:
         trainer._tensorboard.add_train_scalar("standard_evaluate/"+str(j),
                                               smetric[j],
                                               timestep=tid)
-  # Calculate the catastrophic forgetting and add it into tensorboard before
-  # closing the tensorboard
-  c_standard_metric = get_catastrophic_metric(train, ostandard_metrics)
-  print("Forgetting Results", c_standard_metric)
-  for tid,task in enumerate(c_standard_metric, 1):
-      trainer._tensorboard.add_train_scalar("forgetting_metric/standard_" + task,
-					    c_standard_metric[task],
-					    timestep=tid)
-  if not args.no_save_weight:
-      #save_weight.write_activations(overall_metrics, trainer, tasks)
-      save_weight.get_task_tsne(trainer)
+# Calculate the catastrophic forgetting and add it into tensorboard before
+# closing the tensorboard
+c_standard_metric = get_catastrophic_metric(train, ostandard_metrics)
+print("Forgetting Results", c_standard_metric)
+for tid,task in enumerate(c_standard_metric, 1):
+  trainer._tensorboard.add_train_scalar("forgetting_metric/standard_" + task,
+                                        c_standard_metric[task],
+                                        timestep=tid)
+if not args.no_save_weight:
+  #save_weight.write_activations(overall_metrics, trainer, tasks)
+  save_weight.get_task_tsne(trainer)
 
-  trainer._tensorboard._train_log.close()
+trainer._tensorboard._train_log.close()
 
 print("Training Results are on these Arguments", args)
 eva.print_evaluate_stats(train, evaluate_tasks, args, overall_metrics, task_code, experiment)
