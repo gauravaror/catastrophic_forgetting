140497e0926454ee31b6c2d413427df86241ad08
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-03-22 14:03:33 +1100

    New create script for all architectures and all tasks changes to forgetting script

--- script_count_forgetting.sh
+++ script_count_forgetting.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grep Forgetting $1  | awk '{print $4}' | awk -F, ' { sum+=$1; count+=1} END {print "Sum "sum" Count  " count"  Forgetting "sum/count}'
+logfile=$1
+grep Forgetting $1  | awk '{print $4}' | awk -F, -v logfile=$logfile ' { sum+=$1; count+=1} END {print "Forgetting "logfile" Sum "sum" Count  " count"  Forgetting "sum/count}'
